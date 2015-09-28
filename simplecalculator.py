5c5d3babceca072b75bfd77a51bf253c481437c3
Andrew McCune <andrewmccune@Alveyworld-06.local>
2015-09-28 10:28:25 -0600

    9-28-45

---
+++ simplecalculator.py
@@ -0,0 +1,20 @@
+#simple calculator
+import math
+
+def add(x, y):
+	return x + y
+	
+def sub(x, y):
+	return x - y
+	
+def div(x, y):
+if y == 0 
+		return "undefined"
+	return x / y
+	
+def mul(x, y)
+	return x * y
+	
+def main()
+	print 'Simple Caculator'
+	
